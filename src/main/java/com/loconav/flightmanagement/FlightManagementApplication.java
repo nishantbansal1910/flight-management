3b69aa73b8e29a6cba95ff73b53a3aa4cb554857
nishantbansal1910 <nishant.bansal@loconav.com>
2023-07-16 18:16:27 +0530

    first commit

---
+++ src/main/java/com/loconav/flightmanagement/FlightManagementApplication.java
@@ -0,0 +1,14 @@
+package com.loconav.flightmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagementApplication.class, args);
+		System.out.println("application started");
+	}
+
+}
